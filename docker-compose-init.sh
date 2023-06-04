8797fc6fed719064899416b7ec606aeb2b99a3fc
David Erel <david@piiano.com>
2023-06-04 10:45:27 +0300

    creating keys dir

--- docker-compose-init.sh
+++ docker-compose-init.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "Running docker-compose-init.sh"
+
 docker compose exec server-python-django python wait_for_db.py
 docker compose exec server-python-django python manage.py migrate
 docker compose exec server-python-django python manage.py loaddata init.json
 docker compose exec piiano-vault sh -c "cd /usr/init && ./init.sh"
 docker compose exec kibana /usr/bin/create_index_pattern.sh
+
+echo "Finished docker-compose-init.sh"
