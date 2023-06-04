74f04b985fad19a3c43ec0cfaf59cd771200e8bd
David Erel <david@piiano.com>
2023-06-04 19:00:38 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,10 +8,12 @@ wait_until_containers_are_up()
 
     start_time=$(date +%s)
     timeout=$((start_time + 300))  # 5 minutes timeout
+    debug_timeout=$((start_time + 60))  # 1 minutes timeout
 
     while true; do
         # Get the status of each service defined in the Docker Compose file
         container_status=$(docker-compose ps -q)
+        current_time=$(date +%s)
 
         # Check if all containers are running
         if [ -n "$container_status" ]; then
@@ -28,7 +30,10 @@ wait_until_containers_are_up()
                     if [ "$health_status" != "\"running\"" ]; then
                         all_running=false
                         echo "${name} still down $health_status ..."
-                        docker-compose logs
+                        if [ "$current_time" -gt "$debug_timeout" ]; then
+                            echo "Enabling Debug after 1 minute"
+                            docker-compose logs
+                        fi
                         break
                     fi
                 fi
@@ -41,7 +46,6 @@ wait_until_containers_are_up()
         fi
 
         # Check if timeout has been reached
-        current_time=$(date +%s)
         if [ "$current_time" -gt "$timeout" ]; then
             echo "Timeout reached. Containers did not start within 5 minutes."
             docker ps
@@ -66,6 +70,7 @@ check_log_for_pattern()
     while [ $attempt -le $max_attempts ]; do
         # Check if the pattern exists in the Docker log. Notice usage of docker compose logs
         # regular docker logs open a new process so you can't redirect stdout easily
+        echo "Checking for $pattern in $service_name"
         if docker compose logs | grep $service_name | grep -q "$pattern"; then
             echo "Pattern found in $service_name!"
             return 0
@@ -88,6 +93,20 @@ wait_until_vault_is_up()
     check_log_for_pattern "vault-demo-piiano-vault-1" "Serving HTTP on 0.0.0.0:8123"
     if [ "$?" -eq 1 ] ; then
         exit 1
+    fi
+}
+
+check_vault_health()
+{
+    echo "Vault healthcheck..."
+    d=`curl --no-progress-meter http://localhost:8123/api/pvlt/1.0/ctl/info/health | jq -r ".status"`
+    c=`curl --no-progress-meter http://localhost:8123/api/pvlt/1.0/data/info/health | jq -r ".status"`
+
+    if [[ $d != "pass" ]] || [[ $c != "pass" ]]; then
+        echo "Failed healhcheck: data $d control $c"
+        exit 1
+    else
+        echo "Vault healthcheck passed"
     fi
 }
 
@@ -127,6 +146,7 @@ fi
 
 wait_until_containers_are_up
 wait_until_vault_is_up
+check_vault_health
 wait_until_react_is_up
 check_web_is_answering
 docker compose down
